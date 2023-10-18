5504a014681fe3e1a39f801b8f1a927d41ff18fc
Neil Mispelaar <neil.mispelaar@outlook.com>
2023-10-18 10:56:04 -0400

    Lecture 12 setup

---
+++ lecture-12/final/javascript/script.js
@@ -0,0 +1,67 @@
+//
+// Basic Variables
+//
+
+// String
+let myString = "Hello World";
+
+// Number
+
+// Boolean
+
+// Array
+
+// Objects (or object literals)
+
+// Null
+
+// Undefined
+
+
+
+//
+// Operators
+//
+
+// Arithmetic
+// Add to string together
+
+// Add two numbers together
+
+// Subtract two numbers
+
+
+//
+// Conditional Logic
+// using operators
+//
+
+// if
+
+// if else
+
+// if else if else
+
+// switch
+
+
+//
+// Loops
+//
+
+// for
+
+// while
+
+// do while
+
+
+//
+// Functions
+//
+
+// function declaration
+
+// Variable scope (global vs local)
+
+// Outer variables
---
+++ lecture-12/start/javascript/script.js
@@ -0,0 +1,66 @@
+//
+// Basic Variables
+//
+
+// String
+
+// Number
+
+// Boolean
+
+// Array
+
+// Objects (or object literals)
+
+// Null
+
+// Undefined
+
+
+
+//
+// Operators
+//
+
+// Arithmetic
+// Add to string together
+
+// Add two numbers together
+
+// Subtract two numbers
+
+
+//
+// Conditional Logic
+// using operators
+//
+
+// if
+
+// if else
+
+// if else if else
+
+// switch
+
+
+//
+// Loops
+//
+
+// for
+
+// while
+
+// do while
+
+
+//
+// Functions
+//
+
+// function declaration
+
+// Variable scope (global vs local)
+
+// Outer variables
