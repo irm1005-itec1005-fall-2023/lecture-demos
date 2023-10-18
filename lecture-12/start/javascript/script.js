3f4ac7b84f1c1a68bf8bf94a234ae369978fadd0
Neil Mispelaar <neil.mispelaar@outlook.com>
2023-10-18 12:25:10 -0400

    In the middle of class

--- lecture-12/start/javascript/script.js
+++ lecture-12/start/javascript/script.js
@@ -1,16 +1,42 @@
+// Welcome to JavaScript!
+
+/*
+
+JavaScript is a programming language that is used to make web pages interactive. It is a scripting language, which means that it is interpreted by the browser as it is loaded. It is not compiled like Java or C++.
+
+*/
+
 //
 // Basic Variables
 //
 
+
 // String
+let myFirstName = "Neil";
 
 // Number
+let myNumber = 5;
 
 // Boolean
+let isColdOutside = true;
 
 // Array
+let myArray = ["one", "two", "three", "four", "five"];
+
+console.log(myFirstName);
+console.log(myNumber);
+console.log(isColdOutside);
+console.log(myArray);
+
+myNumber = myNumber + 10;
+
+console.log("This is my number after adding ten: " , myNumber);
+
+console.log(myArray[5])
 
 // Objects (or object literals)
+
+
 
 // Null
 
