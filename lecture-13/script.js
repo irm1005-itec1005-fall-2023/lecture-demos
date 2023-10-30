a872bad8b2a4f7a95394d7d06a5e106b8ac3f769
Neil Mispelaar <neil.mispelaar@outlook.com>
2023-10-30 08:17:45 -0400

    Add lecture 13 starter code

---
+++ lecture-13/script.js
@@ -0,0 +1,155 @@
+/*
+ * Lecture 13: JavaScript Basics
+ *
+ * Topics:
+ *  Variables
+ *  Arrays
+ *  Conditionals
+ *  For loops
+ *  Object literals
+ *  Functions
+ */
+
+// Welcome to JavaScript!
+
+//
+// Console log
+//
+
+// Console log is a function that prints to the console
+
+
+//
+// Basic Variables
+//
+
+// Let's create a variable using the let keyword
+
+// String
+
+// Number
+
+// Boolean
+
+// Array
+
+
+//
+// Constant Variables
+//
+
+// Let's create a variable using the const keyword
+
+// String
+
+// Number
+
+// Boolean
+
+// Array
+
+// Try to reassign it and you can't! It's a constant!
+
+
+
+//
+// Working with arrays
+//
+
+
+// Create an array of strings
+
+
+// Use the length property to show the number of items in the array
+
+
+// Use the array[] operator to access an item in the array
+
+
+// Use the array.push() operator to add an item to the end of the array
+
+
+// Use the array.pop() operator to remove an item from the end of the array
+
+
+
+//
+// Conditional logic using operators
+//
+
+// Let's create two variables and compare them using operators
+
+
+// if statement - if the condition is true, run the code inside the block (the curly braces)
+
+
+// if else statement - if the condition is true, run the code inside the first block (the curly braces),
+//  otherwise run the code inside the second block (the curly braces)
+
+
+// if else if else statement - if the first condition is true, run the code inside the first block (the curly braces),
+//  otherwise if the second condition is true, run the code inside the second block (the curly braces),
+//  otherwise run the code inside the third block (the curly braces)
+
+
+
+//
+// For loops
+//
+
+// Let's create a for loop that prints the numbers 1 through 10
+
+
+// Let's create an array and loop through it
+// First we decalre an array of strings
+
+// Then we loop through the array and print each string to the console
+
+// Let's create a new array of strings and find a specific index of an item in the array
+
+// First we decalre an array of strings
+
+// Then we loop through the array and print each string to the console as well as the index of the item
+
+// Then we use an if statement to check if the current item is the one we're looking for
+
+
+
+//
+// Object literals
+//
+
+// Variables like this aren't great
+let productPrice = 10.99;
+let productTitle = "Margherita Pizza";
+let productDescription = "A delicious pizza with tomato sauce and fresh mozzarella cheese";
+let productIngredients = ["tomato sauce", "fresh mozzarella cheese", "basil", "olive oil"];
+let productIsInStock = true;
+
+// Instead, let's create an object literal
+
+
+// Use an if statement to see if the product is in stock
+
+
+// Use a for loop to print the ingredients to the console
+
+
+
+//
+// Functions
+//
+
+// Functions are reusable blocks of code that can be called multiple times
+
+
+// Let's create a function that prints a message to the console
+
+
+// Let's call the function
+
+
+// Let's create a function that takes in a name and prints a message to the console
+
+
+// Let's call the function
