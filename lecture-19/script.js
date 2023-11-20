ad7f9d6945d4defe47259b23f24194ece44dbfa5
Neil Mispelaar <neil.mispelaar@outlook.com>
2023-11-20 11:03:35 -0500

    Lecture 19 starter files

---
+++ lecture-19/script.js
@@ -0,0 +1,24 @@
+/*
+ * Lecture 19: Working with the DOM and Events
+ *
+ * Topics:
+ *  Let's build out our Superhero Name App
+ */
+
+// Welcome to JavaScript!
+
+// Console log is a function that prints to the console
+console.log("🥰 Lecture 19");
+
+//
+// Grab data from a form and display it on the page
+// Put the data into an array and then display the array
+//
+
+// Create our variables
+
+// Handle Submit Form
+
+// Render the data to the page
+
+// Call renderlist for the first time
