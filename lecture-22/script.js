13201eb37c7d2cf987f322e0709cd08b1238ef2b
Neil Mispelaar <neil.mispelaar@outlook.com>
2023-11-29 02:30:28 -0500

    Add starter code for lecture 22

---
+++ lecture-22/script.js
@@ -0,0 +1,46 @@
+/*
+ * Lecture 21: Working with Promises and Fetch
+ *
+ * Topics:
+ *  Let's make a promise
+ *  Let's fetch some data
+ */
+
+// Welcome to JavaScript!
+
+// Console log is a function that prints to the console
+console.log("🥰 Lecture 22");
+
+// Create our variables
+const API_URL = "https://pokeapi.co/api/v2/pokemon/";
+
+// The form
+// The input number
+// The empty state
+// The container
+// Pokemon data from the api
+
+// Render the pokemon item
+
+// Add our event handler for the form submit
+
+// Add our event listener
+
+
+// Fetch the data from the API
+// fetch(`${API_URL}${pokemonNumberToSearch}`)
+// .then((response) => {
+//   // We can use .json() to get the json data from the response
+//   if (response.status === 404) {
+//     throw new Error("Pokemon not found");
+//   }
+
+//   return response.json();
+// })
+// .then((data) => {
+
+//   console.log(data)
+// })
+// .catch((error) => {
+//   console.log(error);
+// });
